a4d41973b2bda951cea8b9f94459ffcb78a88ed3
mikygaleano <mikebostero.bernal@gmail.com>
2024-04-22 15:44:04 -0300

    animacion terminada, cambio el nombre de moveUp a moveParticles, aun falta agregarles las imagenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,13 @@ module.exports = {
 	theme: {
 	  extend: {
 		keyframes: {
-			moveUp: {
-				'0%': {bottom: '0', opacity: '0'},
-				'10%': {bottom: '100px', opacity: '1'},
-				'50%': {bottom: '200px'},
-				'98%': {bottom: '520px', opacity: '0'},
-				'100%': {bottom: '+100%', opacity: '0'}
+			moveParticles: {
+				'0%': {opacity: '0'},
+				'10%': {opacity: '1'},	
 			}
 		},
 		animation: {
-			moveUp: 'moveUp 1s linear infinite'
+			moveParticles: 'moveParticles 1s linear infinite'
 		}
 	  },
 	},
